20d6722ad1a148d6ededf4157d009342626e5aba
Glitch (hello-express) <none>
2020-05-13 22:48:25 +0000

    ❄️🚓 Checkpoint ./server.js:379177/2055 ./public/index.html:379177/3539

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
+const express = require('express');
 const app = express();
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+app.enable('trust proxy');
+app.use(function (req, res, next) {
+  if (req.secure) {
+    return next();
+  }
+  res.set('Strict-Transport-Security', 'max-age=63072000; includeSubDomains; preload');
+  res.redirect('https://' + req.headers.host + req.url);
 });
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+app.use(express.static('public',
+  {
+    maxAge: '1d',
+    setHeaders: (res, path, stat) => {
+      res.set('Strict-Transport-Security', 'max-age=63072000; includeSubDomains; preload');
+    }
+  }
+));
+
+// app.get('/', (req, res) => {
+//   res.set('Strict-Transport-Security', 'max-age=63072000; includeSubDomains; preload');
+//   res.sendFile(__dirname + '/views/index.html');
+// });
+
+const listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
