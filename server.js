c873ed943ced610ea3baa8ad96a01b45d6544779
Glitch (user-agent-client-hints) <none>
2020-09-01 10:21:50 +0000

    🎽🔖 Checkpoint ./public/index.html:379177/5315 ./.glitch-assets:379177/43 ./README.md:379177/772 ./package.json:379177/227 ./server.js:379177/2666

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
-const _HINTS = [
-  'UA-Arch',
-  'UA-Full-Version',
-  'UA-Mobile',
-  'UA-Model',
-  'UA-Platform-Version',
-  'UA-Platform',
-  'UA',
-];
-
 const express = require('express');
 const app = express();
 
-const mustacheExpress = require('mustache-express');
-app.engine('html', mustacheExpress());
- 
-app.set('view engine', 'html');
-app.set('views', __dirname + '/public');
-app.set('view cache', true);
+// If you need some basic templating, then you can enable Mustache
 
+/*
+ * If you need basic templating, you can enable Mustache here
+ * Personal preference is just to "upgrade" existing HTML files with templated variables
+ * Enabling the view cache after the demo is published
+ */
+// const mustacheExpress = require('mustache-express');
+// app.engine('html', mustacheExpress()); 
+// app.set('view engine', 'html');
+// app.set('views', __dirname + '/public');
 
+// app.set('view cache', true);
+
+// Redirect requests to HTTPS by default and set the HSTS header.
+// You will need to disable or modify this if your demo requires plain HTTP
 app.enable('trust proxy');
 app.use(function (req, res, next) {
   if (req.secure) {
@@ -29,49 +27,12 @@ app.use(function (req, res, next) {
   res.redirect(301, 'https://' + req.headers.host + req.url);
 });
 
-app.get('/', (req, res) => {
-  let header = '[not set]';
-  
-  if (req.query.noheader != 'noheader') {
-    let rawCH = [];
-    
-    if (typeof req.query.uach === 'string') {
-      rawCH = [req.query.uach];
-    } else if (Array.isArray(req.query.uach)) {
-      rawCH = req.query.uach;
-    }
+// TODO - if you need any server-side routes, add them here
 
-    const acceptCH = [];
-
-    rawCH.forEach((uach) => {
-      if (_HINTS.indexOf(uach) >= 0) {
-        acceptCH.push(uach);
-      }
-    });
-
-    res.set('Accept-CH', acceptCH.join(', '));
-    header = 'Accept-CH: ' + acceptCH.join(', ');
-  }
-
-  res.render('index', { header: header });
-});
-
-app.get('/show-headers.json', (req, res) => {
-  res.json({
-    'Accept-CH': res.get('Accept-CH'),
-    'Sec-CH': {
-      'UA-Arch': req.get('Sec-CH-UA-Arch'),
-      'UA-Model': req.get('Sec-CH-UA-Model'),
-      'UA-Platform': req.get('Sec-CH-UA-Platform'),
-      'UA-Platform-Version': req.get('Sec-CH-UA-Platform-Version'),
-      'UA': req.get('Sec-CH-UA'),
-      'UA-Full-Version': req.get('Sec-CH-UA-Full-Version'),
-      'UA-Mobile': req.get('Sec-CH-UA-Mobile'),
-    }
-  });  
-});
-
-app.use(express.static('public', { maxAge: '1d' } ));
+// By default, fall back to serving from the `public` directory
+app.use(express.static('public'));
+// Once the demo is published, you may want to add some caching headers
+// app.use(express.static('public', { maxAge: '1d' } ));
 
 const listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
