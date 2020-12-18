d36934dfe10724c76ce122cd633cf8bb0abb34d2
Glitch (user-agent-client-hints) <none>
2020-12-18 12:38:23 +0000

    ⛩💆 Checkpoint ./package.json:379177/1063 ./server.js:379177/3209 ./README.md:379177/682

--- README.md
+++ README.md
@@ -2,5 +2,28 @@
 
 A basic Express-based project with optional Mustache templating.
 
-
 🐦 Ping <https://twitter.com/rowan_m> with questions.
+
+## Running the demo locally
+
+Before the initial run, you will need to download the dependencies from from
+[npm](https://docs.npmjs.com/). In this directory, run:
+
+```
+npm ci
+```
+
+Start the server using:
+
+```
+npm start
+```
+
+The server will run on a random port which will be shown in the console.
+
+If you're developing, then there is a task to start the server in development
+mode on port 8080.
+
+```
+npm run start:dev
+```
--- server.js
+++ server.js
@@ -1,40 +1,60 @@
 const express = require('express');
 const app = express();
 
-// If you need some basic templating, then you can enable Mustache
-
 /*
  * If you need basic templating, you can enable Mustache here
  * Personal preference is just to "upgrade" existing HTML files with templated variables
  * Enabling the view cache after the demo is published
  */
-// const mustacheExpress = require('mustache-express');
-// app.engine('html', mustacheExpress()); 
-// app.set('view engine', 'html');
-// app.set('views', __dirname + '/public');
-// app.set('view cache', true);
+const mustacheExpress = require('mustache-express');
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/public');
+
+if (app.get('env') === 'production') {
+  app.set('view cache', true);
+}
+
+// Allow server to run correctly behind a proxy
+app.enable('trust proxy');
 
 /*
  * Redirect requests to HTTPS by default and set the HSTS header.
  * You will need to disable or modify this if your demo requires plain HTTP
  */
-app.enable('trust proxy');
 app.use(function (req, res, next) {
+  // Allow http://localhost
+  if (req.secure === false && req.hostname === 'localhost') {
+    return next();
+  }
+
+  // Set the HSTS header if we're already on HTTPS
   if (req.secure) {
     res.set('Strict-Transport-Security', 'max-age=63072000; inlcudeSubdomains; preload');
     return next();
   }
-  
+
+  // Otherwise redirect to HTTPS
   res.redirect(301, 'https://' + req.headers.host + req.url);
 });
 
 // TODO - if you need any server-side routes, add them here
+// app.get('/', (req, res) => {
+//   res.render('index');
+// });
 
 // By default, fall back to serving from the `public` directory
 app.use(express.static('public'));
-// Once the demo is published, you may want to add some caching headers
-// app.use(express.static('public', { maxAge: '1d' } ));
+
+// Cache static files in production
+if (app.get('env') === 'production') {
+  app.use(express.static('public', { maxAge: '1d' }));
+}
 
 const listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
+
+  if (app.get('env') === 'development') {
+    console.log('If you are running locally, try http://localhost:' + listener.address().port);
+  }
 });
