0671fe725646bcef6f09a74f6bc2d5e38180811f
Rowan Merewood <rowan@merewood.org>
2026-03-01 08:17:29 +0000

    Clean up console noise

--- src/main.js
+++ src/main.js
@@ -683,9 +683,7 @@ run();
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then(registration => {
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }).catch(err => {
+    navigator.serviceWorker.register('/sw.js').catch(err => {
       console.error('ServiceWorker registration failed: ', err);
     });
   });
