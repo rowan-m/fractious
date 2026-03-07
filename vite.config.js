b495a9b3df7945adb402a13ebdd143c478ff14fb
Rowan Merewood <rowan@merewood.org>
2026-03-07 20:41:41 +0000

    Maybe I got the cross-origin isolation headers right this time

--- vite.config.js
+++ vite.config.js
@@ -10,12 +10,14 @@ export default defineConfig({
   server: {
     headers: {
       'Cross-Origin-Embedder-Policy': 'credentialless',
+      'Cross-Origin-Opener-Policy': 'same-origin',
       'Cross-Origin-Resource-Policy': 'cross-origin'
     }
   },
   preview: {
     headers: {
       'Cross-Origin-Embedder-Policy': 'credentialless',
+      'Cross-Origin-Opener-Policy': 'same-origin',
       'Cross-Origin-Resource-Policy': 'cross-origin'
     }
   }
